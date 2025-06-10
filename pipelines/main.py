2c3d6a1b03b13b2e649368da03babc5b00ba891e
Carlos Pereira de Castro Neto <72046107+carlospcneto@users.noreply.github.com>
2025-06-10 00:27:00 -0300

    fix service count

--- pipelines/main.py
+++ pipelines/main.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     parser.add_argument("--project", type=str, default="boti-project-cpcn", help="ID do projeto GCP")
     parser.add_argument("--region", type=str, default="us-central1", help="Região do AI Platform")
     parser.add_argument("--pipeline-root", type=str, default="gs://boti-project-cpcn/pipelines", help="Caminho raiz do pipeline no Cloud Storage")
-
+    parser.add_argument("--service_account", type=str, help="Conta de serviço para executar o pipeline")
 
     args = parser.parse_args()
     aiplatform.init(
         project=args.project,
         location=args.region,
-        staging_bucket=args.pipeline_root
+        staging_bucket=args.pipeline_root,
+        service_account=args.service_account
     )
 
     job = aiplatform.PipelineJob(
